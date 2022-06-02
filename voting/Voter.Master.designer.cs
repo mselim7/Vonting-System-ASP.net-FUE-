563ba33ab359cfae50546a9303521e534bfdee0d
mahmoudselim07 <73776713+mahmoudselim07@users.noreply.github.com>
2022-06-02 04:54:49 +0200

    new vote

--- voting/Voter.Master.designer.cs
+++ voting/Voter.Master.designer.cs
@@ -24,13 +24,13 @@ namespace voting
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// form2 control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// HyperLink1 control.
@@ -150,12 +150,12 @@ namespace voting
         protected global::System.Web.UI.WebControls.Label lb9;
 
         /// <summary>
-        /// Label9 control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label9;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
